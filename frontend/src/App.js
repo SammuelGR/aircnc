bf55990f46469cd92596dbbaa49f41dd4c8bbee9
Sammuel Reis <sammugr@hotmail.com>
2019-10-04 20:25:57 -0300

    setup simple login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {
+      email,
+    });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -13,11 +28,13 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
         
-        <form >
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             id="email" 
             placeholder="Seu melhor e-mail" 
+            onChange={event => setEmail(event.target.value)}
+            value={email}
             type="email" 
           />
           <button className="btn" type="submit">Entrar</button>
