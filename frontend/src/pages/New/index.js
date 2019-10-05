0a8bc756ddd977c77257907787eb112e196a8ef2
Sammuel Reis <sammugr@hotmail.com>
2019-10-04 21:59:24 -0300

    setup companies upload

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 
 import camera from '../../assets/camera.svg';
 
 import './styles.css';
 
-export default function New() {
+export default function New({ history }) {
     const [thumbnail, setThumbnail] = useState(null);
     const [company, setCompany] = useState('');
     const [techs, setTechs] = useState('');
@@ -14,8 +15,22 @@ export default function New() {
         return thumbnail ? URL.createObjectURL(thumbnail) : null;
     }, [thumbnail]);
     
-    function handleSubmit() {
+    async function handleSubmit(event) {
+        event.preventDefault();
 
+        const data = new FormData();
+        const user_id = localStorage.getItem('user');
+
+        data.append('thumbnail', thumbnail);
+        data.append('company', company);
+        data.append('techs', techs);
+        data.append('price', price);
+
+        await api.post('/spots', data, {
+            headers: { user_id }
+        })
+
+        history.push('/dashboard');
     }
 
     return (
@@ -23,7 +38,7 @@ export default function New() {
             <label 
                 id="thumbnail" 
                 style={{ backgroundImage: `url(${preview})` }}
-                className={thumbnail ? 'has-thumbnail' : ''}
+                className={preview ? 'has-thumbnail' : ''}
             >
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
                 <img src={camera} alt="Select img" />
