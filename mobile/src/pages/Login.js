dc630ceb886c6763dbfc698f326c5d5340d82e66
Sammuel Reis <sammugr@hotmail.com>
2019-10-05 22:56:58 -0300

    implements login

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
+    AsyncStorage,
     Image, 
     KeyboardAvoidingView, 
     // Platform,
@@ -10,9 +11,35 @@ import {
     View 
 } from 'react-native';
 
+import api from '../services/api'; 
+
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [techs, setTechs] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('List');
+            }
+        })
+    }, []);
+
+    async function handleSubmit() {
+        const response = await api.post('/sessions', {
+            email
+        });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+        await AsyncStorage.setItem('techs', techs);
+        
+        navigation.navigate('List');
+    }
+
     return (
         <KeyboardAvoidingView 
             behavior="padding" 
@@ -30,6 +57,8 @@ export default function Login() {
                     keyboardType="email-address"
                     autoCapitalize="none"
                     autoCorrect={false}
+                    value={email}
+                    onChangeText={setEmail}
                 />
 
                 <Text style={styles.label}>TECNOLOGIAS *</Text>
@@ -39,9 +68,10 @@ export default function Login() {
                     placeholderTextColor="#999"
                     autoCapitalize="words"
                     autoCorrect={false}
+                    onChangeText={setTechs}
                 />
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity onPress={handleSubmit} style={styles.button}>
                     <Text style={styles.buttonText}>Encontrar Spots</Text>
                 </TouchableOpacity>
             </View>
