4453604ecaf7074190c20c9e9f8ae6d369ff130c
Sammuel Reis <sammugr@hotmail.com>
2019-10-06 15:22:51 -0300

    setup booking

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,47 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { AsyncStorage, Image, Platform, ScrollView, StyleSheet, SafeAreaView } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, [])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image 
+            style={[styles.logo, Platform.OS === 'ios' ? styles.logoIOS : styles.logoANDROID ]}
+            source={logo}
+            />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    logo: {
+        height:32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
+    logoIOS: {
+        marginTop: 10
+    },
+    logoANDROID: {
+        marginTop: 40
+    }
+});
